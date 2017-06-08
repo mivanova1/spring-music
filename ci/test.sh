9dba3233efd9a0fe24dc1ed465dd8f6cc190ce57
Killian Nolan <killian.nolan@mycit.ie>
2017-06-08 16:01:42 +0100

    kn

--- ci/test.sh
+++ ci/test.sh
@@ -3,8 +3,8 @@ set -e
 
 cd spring-music
 ./gradlew clean assemble
-echo "git init""
-echo "git remote add github https://github.com/Kill93/spring-music
+echo "git init"
+echo "git remote add github https://github.com/Kill93/spring-music"
 echo "git add ."
 echo "git commit -m "hi""
 echo "git push github master"
