7f74e2b9372022ceffb2da194f645a440412749a
Killian Nolan <killian.nolan@mycit.ie>
2017-06-08 15:45:39 +0100

    hi

--- ci/test.sh
+++ ci/test.sh
@@ -3,9 +3,9 @@ set -e
 
 cd spring-music
 ./gradlew clean assemble
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
+apt-get update
+apt-get upgrade
+apt-get install git
 git init
 git remote add github https://github.com/Kill93/spring-music
 git add .
