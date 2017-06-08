0f84b93960874018ddeacbe0b7f7a5a3af4b69d4
Killian Nolan <killian.nolan@mycit.ie>
2017-06-08 10:27:36 +0100

    Update test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+export GRADLE_OPTS=-Dorg.gradle.native=false
+version=`cat version/number`
 cd spring-music
+#echo $version
+gradle assemble -PVersion=$version
+#ls build/libs/
