1b2015adabfb44b9dd4d7a2c01a7faab29018762
mivanova1 <mivanova@pivotal.io>
2017-06-14 15:52:08 +0100

    Initial CommitMon

--- ci/test.sh
+++ ci/test.sh
@@ -4,7 +4,7 @@ set -e
 apk update
 apk add git
 
-git clone https://github.com/Kill93/spring-music spring-music2
+git clone https://github.com/mivanova1/spring-music spring-music2
 
 cd spring-music2
 ./gradlew clean assemble
